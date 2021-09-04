b5ced34fb558f382c57c12cb05762f31d58d8ce8
Amppazz <amppa.mughal@gmail.com>
2021-09-04 17:02:12 +0300

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config()
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
